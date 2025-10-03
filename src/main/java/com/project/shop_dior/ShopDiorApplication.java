a5ee773945adfb0df37743d2c25be78070c8fe4d
Trần Thành Long <tranthanhlong@MacBook-Air-cua-Tran-Thanh-Long.local>
2025-10-03 09:40:03 +0700

    update redis

--- src/main/java/com/project/shop_dior/ShopDiorApplication.java
+++ src/main/java/com/project/shop_dior/ShopDiorApplication.java
@@ -2,8 +2,10 @@ package com.project.shop_dior;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class ShopDiorApplication {
 
 	public static void main(String[] args) {
